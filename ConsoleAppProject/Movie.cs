75ff744119db7b0bd156c3c9ee0d263203762012
AllenOtto <kephamoturi@gmail.com>
2023-06-28 20:15:11 +0300

    Created a new class: Movie; created instances of the class

---
+++ ConsoleAppProject/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppProject
+{
+    internal class Movie
+    {
+        Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+        Movie shrek = new Movie("Shrek", "Wilder Achoki", "PG");
+    }
+}
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -13,16 +13,18 @@ namespace ConsoleAppProject
             // Basics of Objects and Classes
 
             // Student Class
-            Student student1 = new Student("Wendy Akinyi", 12, 3.6);
-            Student student2 = new Student("Wilder Achoki", 1, 3.0);
-            Student student3 = new Student("Kepha Moturi", 14, 2.4);
+            //Student student1 = new Student("Wendy Akinyi", 12, 3.6);
+            //Student student2 = new Student("Wilder Achoki", 1, 3.0);
+            //Student student3 = new Student("Kepha Moturi", 14, 2.4);
 
-            Console.WriteLine(student1.name);
-            Console.WriteLine(student1.HasHonors() + "\n");
-            Console.WriteLine(student2.name);
-            Console.WriteLine(student2.HasHonors() + "\n");
-            Console.WriteLine(student3.name);
-            Console.WriteLine(student3.HasHonors() + "\n");
+            //Console.WriteLine(student1.name);
+            //Console.WriteLine(student1.HasHonors() + "\n");
+            //Console.WriteLine(student2.name);
+            //Console.WriteLine(student2.HasHonors() + "\n");
+            //Console.WriteLine(student3.name);
+            //Console.WriteLine(student3.HasHonors() + "\n");
+
+            // Getters and Setters
 
 
 
