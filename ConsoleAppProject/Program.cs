558f50ef6bef087846bfdb6c15c229f544e4b36e
AllenOtto <kephamoturi@gmail.com>
2023-06-28 20:00:33 +0300

    Working with classes and objects; working with object methods; constructors in c#

---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Basics of Objects and Classes
+
+            // Student Class
+            Student student1 = new Student("Wendy Akinyi", 12, 3.6);
+            Student student2 = new Student("Wilder Achoki", 1, 3.0);
+            Student student3 = new Student("Kepha Moturi", 14, 2.4);
+
+            Console.WriteLine(student1.name);
+            Console.WriteLine(student1.HasHonors() + "\n");
+            Console.WriteLine(student2.name);
+            Console.WriteLine(student2.HasHonors() + "\n");
+            Console.WriteLine(student3.name);
+            Console.WriteLine(student3.HasHonors() + "\n");
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppProject/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppProject
+{
+    internal class Student
+    {
+        public string name;
+        public int grade;
+        public double gpa;
+
+        public Student(string aName, int aGrade, double aGpa)
+        {
+            name = aName;
+            grade = aGrade;
+            gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if(gpa > 2.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
